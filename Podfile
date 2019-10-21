78a3db27df1fdfeaf010ab4690924de50b3a3e4d
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-10-20 20:23:26 -0400

    Initial setup of applicatilication
    Added pod 'Firebase/Analytics' into podfile, imported and confiugure Firebase in the AppDelegate.swift

---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'intripid' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for intripid
+
+  # add the Firebase pod for Google Analytics
+  pod 'Firebase/Analytics'
+  # add pods for any other desired Firebase products
+  # https://firebase.google.com/docs/ios/setup#available-pods
+
+
+  target 'intripidTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'intripidUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- intripid/AppDelegate.swift
+++ intripid/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 import CoreData
 
 @UIApplicationMain
@@ -14,11 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
+//  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//    // Override point for customization after application launch.
+//    return true
+//  }
+  
+  // Zoe: Added FirebaseApp.configure()
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    FirebaseApp.configure()
+    
+//    let myDatabase = Database.database().reference()
+    
     return true
   }
+  
 
   func applicationWillResignActive(_ application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -58,7 +70,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           if let error = error as NSError? {
               // Replace this implementation with code to handle the error appropriately.
               // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-               
+
               /*
                Typical reasons for an error here include:
                * The parent directory does not exist, cannot be created, or disallows writing.
