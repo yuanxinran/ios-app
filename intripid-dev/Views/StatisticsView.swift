77edd60e9fc13dfd2460ee935afef904a26906db
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-12-02 18:15:27 -0500

    add default charts into statistics view

--- intripid-dev/Views/StatisticsView.swift
+++ intripid-dev/Views/StatisticsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct StatisticsView: View {
   @ObservedObject private var viewModel : TripViewModel
@@ -30,76 +31,99 @@ struct StatisticsView: View {
   var body: some View {
     ScrollView {
       VStack(alignment: .leading) {
-        Text("My Footsteps")
-          .font(.title)
-          .fontWeight(.bold)
-        //        MapViewControllerWrapper(trips: self.viewModel.trips)
-        //          .frame(height: 300)
         
-        HStack {
-          VStack {
-            Text("Trips")
-              .font(.headline)
-            ZStack {
-              Circle()
-                .fill(Color.yellow)
-              Text(String(self.viewModel.trips.count))
-            }
-          }
-          VStack {
-            Text("Photos")
-              .font(.headline)
-            ZStack {
-              Circle()
-                .fill(Color.yellow)
-              Text(String(self.viewModel.trips.reduce(0) { $0 + $1.photoNum }))
-            }
-          }
-          VStack {
-            Text("Journals")
-              .font(.headline)
-            ZStack {
-              Circle()
-                .fill(Color.yellow)
-              Text(String(self.viewModel.trips.reduce(0) { $0 + $1.journalNum }))
-            }
-          }
-        }
-        .frame(height: 120)
-        .padding(30)
-        
-        Text("Percentages")
-          .font(.title)
-          .fontWeight(.bold)
-        
-        HStack {
+        VStack {
           ZStack {
-            Rectangle()
-              .fill(Color.yellow)
-              .cornerRadius(20)
-            VStack(alignment: .leading) {
-              Text("42%")
-                .font(.subheadline)
-              Text("Continents")
-                .font(.callout)
-                .fontWeight(.bold)
-            }
+            Image("trip_1")
+              .resizable()
+              .frame(height: 150)
+            Spacer()
+            
+            Text("My Footsteps")
+              .font(.title)
+              .fontWeight(.bold)
+              .foregroundColor(.white)
           }
           
-          ZStack {
-            Rectangle()
-              .fill(Color.yellow)
-              .cornerRadius(20)
-            VStack(alignment: .leading) {
-              Text("42%")
-                .font(.subheadline)
-              Text("Countries")
-                .font(.callout)
-                .fontWeight(.bold)
+          HStack {
+            VStack {
+              Text("Trips")
+                .font(.headline)
+              ZStack {
+                Circle()
+                  .fill(Color.yellow)
+                Text(String(self.viewModel.trips.count))
+              }
+            }
+            VStack {
+              Text("Photos")
+                .font(.headline)
+              ZStack {
+                Circle()
+                  .fill(Color.yellow)
+                Text(String(self.viewModel.trips.reduce(0) { $0 + $1.photoNum }))
+              }
+            }
+            VStack {
+              Text("Journals")
+                .font(.headline)
+              ZStack {
+                Circle()
+                  .fill(Color.yellow)
+                Text(String(self.viewModel.trips.reduce(0) { $0 + $1.journalNum }))
+              }
             }
           }
+          .frame(height: 120)
+          .padding(30)
         }
-        .frame(height: 100)
+        
+        VStack {
+          HStack {
+            BarChartView(data: [8,23,54,32,12,37,7,23,43], title: "Line chart", legend: "Full screen", dropShadow: false) // legend is optional, use optional .padding()
+            PieChartView(data: [8,23,54,32], title: "Title", legend: "Legendary", dropShadow: false) // legend is optional
+          }
+          
+          HStack {
+             PieChartView(data: [8,23,54,32], title: "Title", legend: "Legendary", dropShadow: false) // legend is optional
+            BarChartView(data: [8,23,54,32,12,37,7,23,43], title: "Line chart", legend: "Full screen", dropShadow: false) // legend is optional, use optional .padding()
+          }
+        }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+        
+        
+//        Text("Percentages")
+//          .font(.title)
+//          .fontWeight(.bold)
+//
+//        HStack {
+//          ZStack {
+//            Rectangle()
+//              .fill(Color.yellow)
+//              .cornerRadius(20)
+//            VStack(alignment: .leading) {
+//              Text("42%")
+//                .font(.subheadline)
+//              Text("Continents")
+//                .font(.callout)
+//                .fontWeight(.bold)
+//            }
+//          }
+//
+//          ZStack {
+//            Rectangle()
+//              .fill(Color.yellow)
+//              .cornerRadius(20)
+//            VStack(alignment: .leading) {
+//              Text("42%")
+//                .font(.subheadline)
+//              Text("Countries")
+//                .font(.callout)
+//                .fontWeight(.bold)
+//            }
+//          }
+//        }
+//        .frame(height: 100)
+        
       }
     }.onAppear(perform: self.refresh)
   }
